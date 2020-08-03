ab513fa46b96f645b7a2135c8a43771299c057af
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-08-03 11:52:47 -0400

    test using a-frame version from AR.js and using a-nft

--- pages/index.js
+++ pages/index.js
@@ -4,41 +4,77 @@ import Head from 'next/head'
 const HomePage = () => {
   const [appRendered, setAppRendered] = useState(false);
   useEffect(() => {
-    require('aframe');
+    // require('aframe');
     setAppRendered(true);
   }, []);
 
   return (
-    <div style={{ height: '100%', width: '100%' }}>
+    <>
       <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <title>A-Frame Next.js AR.js</title>
+
+          <script src='https://cdn.jsdelivr.net/gh/aframevr/aframe@1c2407b26c61958baa93967b5412487cd94b290b/dist/aframe-master.min.js'></script>
+          <script src='https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js'></script>
       </Head>
 
       {appRendered &&
-        <a-scene>
-          <a-box
-            position="-1 0.5 -3"
-            rotation="0 45 0"
-            color="#4CC3D9"
-            depth="0.25"
-            height="0.25"
-            width="0.5"
-          />
-          <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E" />
-          <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D" />
-          <a-plane
-            position="0 0 -4"
-            rotation="-90 0 0"
-            width="4"
-            height="4"
-            color="#7BC8A4"
-          />
-          <a-sky color="#ECECEC" />
-        </a-scene>
+        <>
+          <div className="arjs-loader">
+            <div>Loading, please wait...</div>
+          </div>
+          <a-scene
+            vr-mode-ui="enabled: false;"
+            renderer="logarithmicDepthBuffer: true;"
+            embedded
+            arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
+          >
+            <a-nft
+              type="nft"
+              url="https://arjs-cors-proxy.herokuapp.com/https://avo-content-dev.s3.amazonaws.com/campaign-manager/markers/greenlight/greenlight"
+              smooth="true"
+              smoothCount="10"
+              smoothTolerance=".01"
+              smoothThreshold="5"
+              emitevents="true"
+            >
+              <a-box
+                position="0 0 0"
+                rotation="0 45 0"
+                color="#4CC3D9"
+                depth="0.25"
+                height="0.25"
+                width="0.5"
+              />
+            </a-nft>
+            <a-entity camera />
+          </a-scene>
+        </>
       }
-    </div>
+      <style jsx global>
+        {`
+          .arjs-loader {
+            height: 100%;
+            width: 100%;
+            position: absolute;
+            top: 0;
+            left: 0;
+            background-color: rgba(0, 0, 0, 0.8);
+            z-index: 9999;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .arjs-loader div {
+            text-align: center;
+            font-size: 1.25em;
+            color: white;
+          }
+        `}
+      </style>
+    </>
   );
 };
 
