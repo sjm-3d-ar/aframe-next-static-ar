5939ec6cfec9c33e30e153082e3c096b364840fe
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-08-03 13:54:17 -0400

    remove aframe package, use same scripts as AR.js example

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Head from 'next/head'
 
 const HomePage = () => {
-  const [appRendered, setAppRendered] = useState(false);
+  const [clientSideRendered, setClientSideRendered] = useState(false);
+
   useEffect(() => {
-    // require('aframe');
-    setAppRendered(true);
+    setClientSideRendered(true);
   }, []);
 
   return (
     <>
-      {appRendered &&
-        <a-scene>
-          <a-box position="0 1 -3" rotation="0 45 0" color="tomato" depth="0.5" height="0.5" width="0.5"></a-box>
+      {clientSideRendered &&
+        <a-scene
+          vr-mode-ui="enabled: false;"
+          renderer="logarithmicDepthBuffer: true;"
+          embedded
+          arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;"
+        >
+
+          <a-nft
+            type="nft"
+            url="https://arjs-cors-proxy.herokuapp.com/https://avo-content-dev.s3.amazonaws.com/campaign-manager/markers/greenlight/greenlight"
+            smooth="true"
+            smoothCount="10"
+            smoothTolerance="0.01"
+            smoothThreshold="5"
+          >
+            <a-box position="0 1 0" rotation="0 45 0" color="tomato" depth="0.5" height="0.5" width="0.5"></a-box>
+          </a-nft>
+
+          <a-entity camera></a-entity>
+  
         </a-scene>
       }
     </>
